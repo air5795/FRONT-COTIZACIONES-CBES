8aa32b4e3433dbd06d74952b472510c0e2ec2872
ALE-OFI <air5795@gmail.com>
2025-10-22 14:44:47 -0400

    feat: Configurar ambientes desarrollo y producción

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,16 @@
+// DEVELOPMENT ENVIRONMENT
+export const environment = {
+    production: false,
+    url: "http://10.0.0.152:4002/api/v1/",
+    sistema: 47,
+    url_seguridad: "http://10.0.0.152:3009/api/v1/",
+    loginUrlAsus: 'https://test.asuss.gob.bo/asuss-asegurado-api/api/v1/login',
+    consultaUrlAsus: 'https://test.asuss.gob.bo/asuss-asegurado-api/api/v2/interoperabilidad/segip/consultaDatosPersona',
+    userNameAsus: 'cbes',
+    passwordAsus: 'cbes.2022**',
+    urlMSAuth: 'http://10.0.10.217:3000/api/auth/get-session',
+    urlMSAuthToken: 'http://10.0.10.217:3000/api/auth',
+    urlMSAuthRefreshToken: 'http://10.0.10.217:3000/api/',
+    login: 'http://10.0.10.200:4300/',
+    url_imagenes: 'http://10.0.0.152:4002/',
+  };
